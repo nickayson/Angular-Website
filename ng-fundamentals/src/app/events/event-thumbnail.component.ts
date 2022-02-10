42e1405916283a4a47792a6b65b3427f167fdf26
nickayson <aysonnicholas@gmail.com>
2022-02-10 08:03:42 -0800

    ngif

--- ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -8,12 +8,16 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
     <div>Date: {{event?.date}}</div>
     <div>Time: {{event?.time}}</div>
     <div>Price: \${{event?.price}}</div>
-      <div>
+      <div *ngIf="event?.location">
         <span>Location: {{event?.location?.address}}</span>
 
         <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
       </div>
-      <div>Online URL: {{event?.onlineUrl}} </div>
+      <!-- makes online URL not show if empty -->
+      <div *ngIf="event?.onlineUrl">
+        Online URL: {{event?.onlineUrl}}
+      </div>
+
     </div>
   `,
   styles: [`
